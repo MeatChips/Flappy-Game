4883b227ce09481eba198a96b73ae2d6a6b13a40
MeatChips <84376178+MeatChips@users.noreply.github.com>
2022-06-06 12:15:49 +0200

    Flappy Game

---
+++ Flappy Game/Assets/Scripts/ObstacleGen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGen : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    public int genRate = 100;
+
+    private int timer;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer++;
+        if(timer >= genRate)
+        {
+            timer = 0;
+            GameObject newObstacle = Instantiate(obstaclePrefab, new Vector2(obstaclePrefab.transform.position.x, obstaclePrefab.transform.position.y + Random.Range(-2f, 2f)), obstaclePrefab.transform.rotation);
+            Destroy(newObstacle, 10f);
+        }
+    }
+}
